0db1d8ba7c8f1f968707b64953aa264c75bd54aa
anigandini <anigandini@gmail.com>
2024-03-02 04:40:08 -0300

    :lipstick: Implement background pattern for each theme in Home component and ensure proper alignment of its children components.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <ThemeSelector></ThemeSelector>
-  </div>
+  <main class="home" :class="themeColor">
+      <ThemeSelector class="to-bottom"></ThemeSelector>
+  </main>
 </template>
 
 <script lang="ts">
@@ -13,11 +13,42 @@ export default {
     components: {
         ThemeSelector,
     },
+    computed: {
+        ...mapState("theme", ["themeColor"]),
+    },
 };
 </script>
 
 <style lang="scss">
   .home {
+    height: 100%;
+    display: flex;
+	  width: 100%;
+    
+    &.cherry {
+      background: url('../assets/patterns-sprite.png') no-repeat -1470px -595px;
+    }
+
+    &.coconut {
+      background: url('../assets/patterns-sprite.png') no-repeat -10px -633px;
+    }
+
+    &.lemon {
+      background: url('../assets/patterns-sprite.png') no-repeat 5px -1122px;
+    }
+
+    &.peach {
+      background: url('../assets/patterns-sprite.png') no-repeat -187px 299px;
+    }
+
+    &.watermelon {
+      background: url('../assets/patterns-sprite.png') no-repeat -1534px 278px;
+    }
+
+    .to-bottom {
+      align-self: flex-end;
+    }
+
     @media screen and (min-width: 768px){
       section {
         height: 50vh;
