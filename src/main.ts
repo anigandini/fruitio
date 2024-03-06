68a728138f967bc8c24dfc75f6069581f85f04cc
anigandini <anigandini@gmail.com>
2024-03-06 18:46:54 -0300

    :adhesive_bandage: Refactor import route for improved clarity and maintainability.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "@/store";
-import './styles/variables.scss';
+import "@/styles/variables.scss";
 
 new Vue({
   render: (h) => h(App),
